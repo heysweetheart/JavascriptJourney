6660d07fb28bb3a5dd0ba611457feab36c708cff
heysweetheart <mdmamunurrashidhridoy@gmail.com>
2024-11-03 14:06:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) will not work, cause we're using nodejs not browser
+
+console.log(3+3)
+
+console.log("Hey visitor! how do you get into my github?")
+
+let name = "Visitor"
+let age = 18
+let loggedIn = false
+
+// DataTypes are:
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value which represent empty value | typeof will show object
+// undefined => created variable but not assigned any value 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
