a94c0f06a3f9ff19a494144716620f34618e9453
heysweetheart <mdmamunurrashidhridoy@gmail.com>
2024-11-03 14:21:17 +0000

    conversion confusion

---
+++ 01_basics/03_conversion_confusion.js
@@ -0,0 +1,15 @@
+let score = "33"
+
+console.log(typeof(score));
+
+let valueNumber = Number(score)
+
+console.log(typeof(valueNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+// 1 => true ; 0 => false
+// "" => false
+// "dhf" => true
